1f1e03f124aa186ec058ab874c15409fa8daca2b
TOMITA Masahiro <tommy@tmtm.org>
2019-11-23 17:52:29 +0900

    add charset, collation, status

--- tools/run.sh
+++ tools/run.sh
@@ -2,7 +2,9 @@
 set -e
 
 CURDIR=$(pwd)
+mkdir -p $CURDIR/{mysqld,mysql,charset,collation,status}/data
 for tar in "$@"; do
+    echo "[[[[ $tar ]]]]"
     m=$(tar tf $tar | head -1 | cut -d/ -f1)
     ver=$(echo $m | egrep -o '[0-9]+\.[0-9]+\.[0-9]+')
     cd /tmp
@@ -15,7 +17,20 @@ for tar in "$@"; do
     else
         bin/mysqld --no-defaults --initialize
     fi
-    chown -R mysql. /usr/local/mysql
+    chown -R mysql. .
     /usr/local/mysql/bin/mysqld --no-defaults --help -v > $CURDIR/mysqld/data/$ver.txt || true
+    sed -i -e "s/$(hostname)/hostname/g" $CURDIR/mysqld/data/$ver.txt
     /usr/local/mysql/bin/mysql --no-defaults --help -v > $CURDIR/mysql/data/$ver.txt || true
+    /usr/local/mysql/bin/mysqld --no-defaults -umysql --skip-grant-tables &
+    pid=$!
+    while [ ! -e /tmp/mysql.sock ]; do
+        sleep 1
+    done
+    bin/mysql --no-defaults -e 'SHOW CHARSET' > $CURDIR/charset/data/$ver.txt
+    bin/mysql --no-defaults  -e 'SHOW COLLATION' > $CURDIR/collation/data/$ver.txt
+    bin/mysql --no-defaults  -e 'SHOW GLOBAL STATUS' | awk '{print $1}' > $CURDIR/status/data/$ver.txt
+    kill $pid
+    while ps -p $pid; do
+        sleep 1
+    done
 done
--- tools/to_json.rb
+++ tools/to_json.rb
@@ -1,17 +1,35 @@
 #!/usr/bin/env ruby
 
+require 'fileutils'
 require 'pathname'
 require 'json'
 
 ['mysqld', 'mysql'].each do |name|
   vers = {}
   dir = Pathname(__dir__)+'..'+name
+  FileUtils.mkdir_p(dir + 'json')
   (dir + 'data').each_child do |txt|
     ver = txt.basename('.txt').to_s
     vers[ver] = "json/#{ver}.json"
     params = txt.read.split(/^-+ -+\n/).last.split(/^$/).first.lines.map do |line|
       name, value = line.chomp.split(/ +/, 2)
       [name.gsub(/_/, '-'), value]
+    end.to_h
+    (dir + "json/#{ver}.json").write params.to_json
+  end
+  (dir + "json/version.json").write vers.sort_by{|k, v| k.split('.').map(&:to_i) }.reverse.to_h.to_json
+end
+
+['charset', 'collation', 'status'].each do |name|
+  vers = {}
+  dir = Pathname(__dir__)+'..'+name
+  FileUtils.mkdir_p(dir + 'json')
+  (dir + 'data').each_child do |txt|
+    ver = txt.basename('.txt').to_s
+    vers[ver] = "json/#{ver}.json"
+    params = txt.read.lines[1..-1].map do |line|
+      param = line.split.first
+      [param, param]
     end.to_h
     (dir + "json/#{ver}.json").write params.to_json
   end
